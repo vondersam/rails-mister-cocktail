2635ddc30dd95f588cf3319bc07defbd574513c8
Samuel Rodriguez <samuelyret@gmail.com>
2016-11-24 14:12:53 +0100

    finished models validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
+  validates :cocktail, uniqueness: { scope: :ingredient,
+    message: "one dose per cocktail/ingredient couple" }
 end
